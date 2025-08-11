d22cbe8b0f99692762113e828b1cd81d63a326ea
React-ItCam <krot.kostya.krot@gmail.com>
2025-08-12 00:40:23 +0300

    blablabla

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://logo-teka.com/wp-content/uploads/2025/07/alfa-romeo-logo.svg" alt=""/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://avatars.mds.yandex.net/i?id=26239a7c3c6f67b0ea76a562e8c0fe62f328e67b-4820594-images-thumbs&n=13"
+                    alt=""/>
+                <div>
+                    ava + descr
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
